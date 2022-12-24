19de0c370686303d12117ec6369d76f281c62b17
chigurg <arvodes@gmail.com>
2022-12-25 00:59:02 +0100

    ewfe

--- js/main.js
+++ js/main.js
@@ -20,8 +20,27 @@ window.addEventListener('scroll', () => {
 
 
 var aboutButton = document.getElementById("about");
+var aboutPage = document.getElementById("about-page");
+var aboutElements = document.querySelectorAll(".about-elements");
+console.log(aboutElements);
+console.log(aboutElements[0])
+var aboutElementsNum = aboutElements.length
 
 function menu() {
     aboutButton.style.color = "whitesmoke"
-    aboutButton.innerHTML = "<h2 onclick='back()'>about</h2>"
+    aboutButton.innerHTML = "<h2 onclick='back()'>back</h2>"
+    aboutPage.style.transform = "scale(1,1)"
+    for (let i = 0; i < aboutElementsNum; i++){
+        aboutElements[i].style.opacity = "1";
+    }
+}
+
+
+function back() {
+    aboutButton.style.color = "rgb(45, 45, 45)"
+    aboutButton.innerHTML = "<h2 onclick='menu()'>about</h2>"
+    aboutPage.style.transform = "scale(0,0)"
+    for (let i = 0; i < aboutElementsNum; i++){
+        aboutElements[i].style.opacity = "0";
+    }
 }
